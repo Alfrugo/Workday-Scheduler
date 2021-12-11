af9d392c8aa4a3a8e392bfd5e257fb3180e7a701
Alfrugo <adiez@mtm-inc.net>
2021-12-11 10:01:51 -0600

    localStorage works DRY

--- script-scheduler.js
+++ script-scheduler.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
 })
 
 
-
 //select input add data 
 
 $(".saveBtn").on('click', function(){
@@ -19,7 +18,42 @@ $(".saveBtn").on('click', function(){
   console.log ("this: " + this);
   console.log ("This is the text area: " + textArea);
 
-  localStorage.setItem(timeBlock, textArea); // not working yet
-  console.log ("localStorage= " + localStorage.timeBlock);
+  localStorage.setItem(timeBlock, textArea); // added the load manually needs for loop
+  //console.log ("localStorage= " + localStorage[timeBlock, textArea]);
 })
+
+
+// loads localStorage  and 24 hour to 12 hour conversion. needs for loop
+    $("#h8 .description")
+        .val(localStorage
+        .getItem("h8"));
+    console.log("h8="+h8);
+    $("#h9 .description")
+        .val(localStorage
+        .getItem("h9"));
+    $("#h10 .description")
+        .val(localStorage
+        .getItem("h10"));
+    $("#h11 .description")
+        .val(localStorage
+        .getItem("h11"));
+    $("#h12 .description")
+        .val(localStorage
+        .getItem("h12"));
+    $("#h13 .description")
+        .val(localStorage
+        .getItem("h13"));
+    $("#h14 .description")
+        .val(localStorage
+        .getItem("h14"));
+    $("#h15 .description")
+        .val(localStorage
+        .getItem("h15"));
+    $("#h16 .description")
+        .val(localStorage
+        .getItem("h16"));
+    $("#h17 .description")
+        .val(localStorage
+        .getItem("h17"));
+
 
